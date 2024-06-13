35e7d3752d7ed04386cdd3c12bb4b5494f7966c8
FravkingTheCoder <fravking@gmail.com>
2024-06-13 18:08:06 +0530

    pertag patch1

--- config.h
+++ config.h
@@ -7,12 +7,12 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "CaskaydiaMono Nerd Font:size=16" };
 static const char dmenufont[]       = "CaskaydiaMono Nerd Font:size=16";
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#000000";
-static const char col_gray3[]       = "#e2898b";
-static const char col_gray4[]       = "#ffc539";
-static const char col_cyan[]        = "#363c3b";
-static const char *colors[][3]      = {
+static const char col_cyan[]    = "#717776";
+static const char col_gray2[]   = "#000000";
+static const char col_gray3[]   = "#e66a73";
+static const char col_gray4[]   = "#ffc539";
+static const char col_gray1[]   = "#363c3b";
+static const char *colors[][3]  = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_gray4  },
@@ -35,7 +35,9 @@ static const Rule rules[] = {
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int attachbelow = 1;    /* 1 means attach after the currently active window */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
