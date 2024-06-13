c4bd171077c58470ac2d6ca551b218f6e74971ae
FravkingTheCoder <fravking@gmail.com>
2024-06-13 15:56:02 +0530

    changed color

--- config.h
+++ config.h
@@ -11,11 +11,11 @@ static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#000000";
 static const char col_gray3[]       = "#e2898b";
 static const char col_gray4[]       = "#ffc539";
-static const char col_cyan[]        = "#535177";
+static const char col_cyan[]        = "#363c3b";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray1,  col_gray4  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray4  },
 };
 
 /* tagging */
